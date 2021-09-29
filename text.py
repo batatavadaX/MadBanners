7bde2897cb9ff80c4bde4d16f716b0e44414a50c
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-09-29 23:57:37 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,71 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+def write_text():
+    image=Image.open('banner/banner.png')
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('fonts/Roboto-Regular.ttf', size=45)
+    k = message.reply_to_message.text.split("\n")
+    m = k[0]
+
+    #1
+    (x, y) = (210,270)
+    m = k[0]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #2
+    (x, y) = (210,340)
+    m = k[1]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #3
+    (x, y) = (210,410)
+    m = k[2]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #4
+    (x, y) = (210,480)
+    m = k[3]
+    color = 'rgb(255,255,255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #5
+    (x, y) = (210,550)
+    m = k[4]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #6
+    (x, y) = (575,275)
+    m = k[5]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #7
+    (x, y) = (575,340)
+    m = k[6]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #8
+    (x, y) = (575,410)
+    m = k[7]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #9
+    (x, y) = (575,480)
+    m = k[8]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+
+    #10
+    (x, y) = (575,550)
+    m = k[9]
+    color = 'rgb(255, 255, 255)'
+    draw.text((x, y), m, fill=color, font=font)
+    image.save('banner/banner.png')
+    
