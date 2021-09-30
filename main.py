0e10d6cd34af9cdde7289ec635130b276757f008
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-09-30 23:06:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ m = Client("m", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
 @m.on_message(filters.command("savelogo", prefixes="/"))
-async def save_logo(message: Message):
+async def save_logo(Client, message: Message):
   if message.reply_to_message.photo or message.reply_to_message.document:
     k=await message.client.download_media(message=reply_message, file_name="banner/logo.png")
     b=f"Downloaded {k}"
     await message.reply(message.chat_id, b)
     
 @m.on_message(filters.command("setwinners", prefixes="/"))
-async def set_winners(message: Message):
+async def set_winners(Client, message: Message):
   if message.reply_to_message.text:
     k = message.reply_to_message.text.split("\n")
     with open("banner/winners.txt", "wb") as f:
