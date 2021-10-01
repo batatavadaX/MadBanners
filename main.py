19ffcf687fe0501a1898f639dd7344ea66438818
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-10-01 23:28:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def download_file(Client, query: CallbackQuery):
    
    await query.message.reply_text("winner na naam mokal", reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True))
    if message.reply_to_message.text:
-     await message.reply_text(message.reply_to_message.text)
+     await query.message.reply_text(message.reply_to_message.text)
    
 # @m.on_message(filters.text & filters.private & filters.regex(r"^done"))
 
